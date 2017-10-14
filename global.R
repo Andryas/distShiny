54bd999311a43d660ce4e9ec97a9313e3393a025
AW-DESKTOP <andryaas@gmail.com>
2017-10-14 15:12:18 -0300

    add uniform_cnt function

--- global.R
+++ global.R
@@ -65,3 +65,38 @@ exponencial <- function(lambda,x1,x2){
    
  list(g,r = pexp(x2,lambda) - pexp(x1,lambda))
 }
+
+uniforme_cont <- function(a,b,x1,x2){
+  x <- seq(from = a, to = b, length.out = 1000)
+  
+  df <- data.frame(x = x,
+                   y = dunif(x,min = a,max = b))
+  
+  
+  df2 <- data.frame(x = c(x1,seq(from = x1, to = x2, length.out = 1000),x2),
+                    y = c(0,dunif(seq(x1,x2,length.out = 1000),a,b),0))
+  
+  sumy <- (1/(b-a)) * 0.25
+  
+  arrow1 <- rbind(df2[2,],c(df2[2,1],df2[2,2] + sumy))
+  
+  arrow2 <- rbind(df2[nrow(df2)-1,], c(df2[nrow(df2)-1,1],
+                                       df2[nrow(df2)-1,2] + sumy))
+  
+  
+  g <- qplot(x = x, y = y, data = df, geom = "line",xlab = "x", ylab = "f(x)") + 
+    geom_segment(aes(x = a, xend = a, y = 0, yend = 1 / (b - a)),
+                 linetype = "dashed") + 
+    geom_segment(aes(x = b, xend = b, y = 0, yend = 1 / (b - a)),
+                 linetype = "dashed") + 
+    geom_polygon(data = df2, aes(x,y), fill = "blue") + 
+    geom_line(data = arrow1,aes(x = x,y = y),
+        arrow = arrow(length = unit(0.3,"cm"),ends = "first",type = "closed")) + 
+    geom_line(data = arrow2,aes(x = x,y = y),
+        arrow = arrow(length = unit(0.3,"cm"),ends = "first",type = "closed")) + 
+    annotate("text",x = arrow1$x[2], y = arrow1$y[2] + sumy * 0.2, label = x1) +
+    annotate("text",x = arrow2$x[2], y = arrow2$y[2] + sumy * 0.2, label = x2)
+  
+  list(g,r = punif(x2,a,b) - punif(x1,a,b))
+}
+
